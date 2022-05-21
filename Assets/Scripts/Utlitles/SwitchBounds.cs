aa7ace96b0652f0576464910f6e280a79b687cba
WYT <as5818956@gmail.com>
2022-05-21 11:55:49 +0900

    Base Camera Setup

---
+++ Assets/Scripts/Utlitles/SwitchBounds.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class SwitchBounds : MonoBehaviour
+{
+    //TO 切换场景后调用
+    private void Start()
+    {
+        SwitchConfinerShape();
+    }
+
+    private void SwitchConfinerShape()
+    {
+        PolygonCollider2D confinerShaper =
+            GameObject.FindGameObjectWithTag("BoundsConfiner").GetComponent<PolygonCollider2D>();
+
+        CinemachineConfiner confiner = GetComponent<CinemachineConfiner>();
+        confiner.m_BoundingShape2D = confinerShaper;
+        //Call this if the bounding shape's points change at runtime
+        confiner.InvalidatePathCache();
+    }
+}
